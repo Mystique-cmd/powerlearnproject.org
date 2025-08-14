a490a3987f0b7a755fbd5e022ecfc2bd03f9c3bd
Mystique <mystiquesoul777@gmail.com>
2025-08-14 11:38:17 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,28 +1,28 @@
---SQL query to retrieve the checkNumber, paymentDate, and amount from the payments table.
+-- SQL query to retrieve the checkNumber, paymentDate, and amount from the payments table.
 SELECT checkNumber, paymentDate, amount
 FROM payments;
 
---SQL query to retrieve the orderDate, requiredDate, and status of orders that are currently 'In Process' from the orders table. 
---Sort the results in descending order of orderDate
+-- SQL query to retrieve the orderDate, requiredDate, and status of orders that are currently 'In Process' from the orders table. 
+-- Sort the results in descending order of orderDate
 SELECT orderDate, requiredDate, status
 FROM orders
 WHERE status = 'In Process'
 ORDER BY orderDate DESC;
 
 -- query to display the firstName, lastName, and email of employees whose job title is 'Sales Rep' 
---order them in descending order of employeeNumber.
+-- order them in descending order of employeeNumber.
 SELECT firstName, lastName, email
 FROM employees
 WHERE jobTitle = 'Sales Rep'
 ORDER BY employeeNumber DESC;
 
---query to retrieve all the columns and records from the offices table.
+-- query to retrieve all the columns and records from the offices table.
 SELECT *
 FROM offices;
 
 
---query to fetch the productName and quantityInStock from the products table. 
---Sort the results in ascending order of buyPrice and limit the output to 5 records.
+-- query to fetch the productName and quantityInStock from the products table. 
+-- Sort the results in ascending order of buyPrice and limit the output to 5 records.
 SELECT productName, quantityInStock
 FROM products
 ORDER BY buyPrice ASC
